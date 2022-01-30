6a29f6e2c3bb7e3fb57c629eef83ffbe03f348f8
yinka <holayinkajr@gmail.com>
2022-01-30 18:54:00 +0100

    writing http handlers for user

---
+++ api/user.go
@@ -0,0 +1,98 @@
+package api
+
+import (
+	"database/sql"
+	"net/http"
+	"time"
+
+	"github.com/gin-gonic/gin"
+	"github.com/lib/pq"
+	db "github.com/yimikao/reddit-clone/db/sqlc"
+	"github.com/yimikao/reddit-clone/util"
+)
+
+type createUserRequest struct {
+	Username string `json:"username" binding:"required, alphanum"`
+	Password string `json:"password" binding:"required, min=6"`
+	Email    string `json:"email" binding:"required, email"`
+}
+
+type userResponse struct {
+	Username          string    `json:"username"`
+	Email             string    `json:"email"`
+	PasswordChangedAt time.Time `json:"password_changed_at"`
+	CreatedAt         time.Time `json:"created_at"`
+}
+
+type getUserRequest struct {
+	ID int64 `uri:"id" binding:"required, min=1"`
+}
+
+func newUserResponse(u *db.User) userResponse {
+	return userResponse{
+		Username:          u.Username,
+		Email:             u.Email,
+		PasswordChangedAt: u.PasswordChangedAt,
+		CreatedAt:         u.CreatedAt,
+	}
+}
+
+func (s *Server) createUser(ctx *gin.Context) {
+	var req createUserRequest
+	if err := ctx.ShouldBindJSON(&req); err != nil {
+		ctx.JSON(http.StatusBadRequest, errorResponse(err))
+		return
+	}
+
+	hashed_pw, err := util.HashPassword(req.Password)
+	if err != nil {
+		ctx.JSON(http.StatusInternalServerError, errorResponse(err))
+		return
+	}
+
+	args := db.CreateUserParams{
+		Username:       req.Username,
+		HashedPassword: hashed_pw,
+		Email:          req.Email,
+	}
+
+	u, err := s.store.CreateUser(ctx, args)
+	if err != nil {
+		if pqErr, ok := err.(*pq.Error); ok {
+			switch pqErr.Code.Name() {
+			case "unique_violation":
+				ctx.JSON(http.StatusForbidden, errorResponse(err))
+				return
+			}
+		}
+		ctx.JSON(http.StatusInternalServerError, errorResponse(err))
+		return
+	}
+
+	ctx.JSON(http.StatusCreated, newUserResponse(&u))
+}
+
+func (s *Server) getUser(ctx *gin.Context) {
+	var req getUserRequest
+	if err := ctx.ShouldBindJSON(&req); err != nil {
+		ctx.JSON(http.StatusBadRequest, errorResponse(err))
+		return
+	}
+
+	u, err := s.store.GetUser(ctx, req.ID)
+	if err != nil {
+		if err == sql.ErrNoRows {
+			ctx.JSON(http.StatusNotFound, errorResponse(err))
+			return
+		}
+		ctx.JSON(http.StatusInternalServerError, errorResponse(err))
+		return
+	}
+
+	ctx.JSON(http.StatusOK, userResponse{
+		Username:          u.Username,
+		Email:             u.Email,
+		PasswordChangedAt: u.PasswordChangedAt,
+		CreatedAt:         u.CreatedAt,
+	})
+}
