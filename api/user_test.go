ff77a41b72c0dafd4e12d309eb9e1cb7dedb4b2f
yinka <holayinkajr@gmail.com>
2022-02-11 17:18:03 +0100

    writing user tests

---
+++ api/user_test.go
@@ -0,0 +1,89 @@
+package api
+
+import (
+	"bytes"
+	"database/sql"
+	"encoding/json"
+	"io/ioutil"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+
+	"github.com/golang/mock/gomock"
+	"github.com/stretchr/testify/require"
+	mockdb "github.com/yimikao/reddit-clone/db/mock"
+	db "github.com/yimikao/reddit-clone/db/sqlc"
+
+	"github.com/yimikao/reddit-clone/util"
+)
+
+func randomUser(t *testing.T) (u db.User, password string) {
+	password = util.RandomString(6)
+	hpwd, err := util.HashPassword(password)
+	require.NoError(t, err)
+
+	u = db.User{
+		Username:       util.RandomOwner(),
+		HashedPassword: hpwd,
+		Email:          util.RandomString(6),
+		Avatar: sql.NullString{
+			String: util.RandomString(12),
+			Valid:  true,
+		},
+	}
+	return
+}
+
+func requireBodyMatchUser(t *testing.T, body *bytes.Buffer, u *db.User) {
+	bytes, err := ioutil.ReadAll(body)
+	require.NoError(t, err)
+
+	var userGotten db.User
+	err := json.Unmarshal(bytes, &userGotten)
+	require.NoError(t, err)
+
+	require.Equal(t, userGotten, u)
+}
+
+
+func TestGetUserAPI(t *testing.T) {
+	u, _ := randomUser(t)
+
+	testCases := []struct {
+		name          string
+		userID	int64
+		buildStubs    func(s *mockdb.MockStore)
+		checkResponse func(t *testing.T, r *httptest.ResponseRecorder)
+	}{
+		{
+			name: "OK",
+			userID: u.ID,
+			buildStubs: func(s *mockdb.MockStore) {
+				s.EXPECT().
+				GetUser(gomock.Any(), gomock.Eq(u.ID)).
+				Times(1).
+				Return(u, nil)					
+			},
+			checkResponse: func(t *testing.T, r *httptest.ResponseRecorder) {
+				require.Equal(t, http.StatusOK, r.Code)
+				requireBodyMatchUser(t, r.Body, u)
+			},
+		},
+		{
+			name: "Not Found",
+			userID: u.ID,
+			buildStubs: func(s *mockdb.MockStore) {
+				s.EXPECT().
+				GetUser(gomock.Any(), gomock.Eq(u.ID)).
+				Times(1).
+				Return(db.User{}, sql.ErrNoRows)
+			},
+			checkResponse: func(t *testing.T, r *httptest.ResponseRecorder) {
+				require.Equal(t, http.StatusNotFound, r.Code)
+			},
+
+
+		}
+	}
+
+}
